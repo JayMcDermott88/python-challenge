4d3652ec9c9a4581fd022f340f96adbcdee57aab
JayMcDermott88 <jay.mcdermott93@gmail.com>
2018-12-02 21:27:26 -0600

    Add PyBank

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,32 +1,25 @@
-import os
 import csv
 
 csvpath = "/Users/jaybaybay/Downloads/Solved 17/Resources/budget_data.csv"
 
-Total_Months = []
-Total = []
-Average_Change = []
-Greatest_Increase = []
-Greatest_Decrease = []
+def getnumbers(records)
 
-# Method 2: Improved Reading using CSV module
+totalmonths = len(int(row[0]))
+totalPL = sum(int(row[1]))
+averagechange = 
+greatestincrease = 
+greatestdecrease = 
 
-with open(csvpath, newline='') as csvfile:
+Print("Financial Analysis")
+Print("----------------------------------")
+print(f"Total Months: {str(totalmonths)}")
+print(f"Total: {str(totalPL)}")
+print(f"Average Change: {str(averagechange)}")
+print(f"Greatest Increase in Profits: {str(greatestincrease)}")
+print(f"Greatest Decrease in Profits: {str(greatestdecrease)}")
 
-    # CSV reader specifies delimiter and variable that holds contents
+with open(csvpath, 'r') as csvfile:
+
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
-    for row in csvreader:
-        print(row)
-
-        jay
-
-
-
+    header = next(csvreader)
