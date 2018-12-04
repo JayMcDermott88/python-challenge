980a979a39d31da024c87cb14aa83bd0e1a49621
JayMcDermott88 <jay.mcdermott93@gmail.com>
2018-12-03 21:59:21 -0600

    Add PyPoll

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,1 +1,15 @@
-test 
+import csv
+
+# Path to connect data
+csvpath = "/Users/jaybaybay/python-challenge/PyPoll/election_data.csv"
+
+# read in CSV file
+with open(csvpath, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    for row in csvreader
+        print(row)
